8a58b2871a4257d6e7c63ee71932721f4c0c39e5
TrevorWu <wowpapa3232@gmail.com>
2021-01-04 08:17:23 +0800

    Fetch value from database

--- project/views/general.py
+++ project/views/general.py
@@ -1,28 +1,47 @@
-import adafruit_dht
-import board
+# import adafruit_dht
+# import board
 from flask import Blueprint, render_template, session, redirect, url_for, \
     request, flash, g, jsonify, abort, send_file
-import hashlib
-import tempfile
+import pymysql.cursors
 
 mod = Blueprint('general', __name__)
 
-dhtDevice = adafruit_dht.DHT11(board.D14)
+# dhtDevice = adafruit_dht.DHT11(board.D14)
 
-@mod.route('/', methods=['GET', 'POST'])
+@mod.route('/', methods=['GET'])
 def index():
     if request.method == 'GET':
+        global result
         global temperature_c
         global humidity
         try:
-            temperature_c = dhtDevice.temperature
-            humidity = dhtDevice.humidity
+            # Connect to the database
+            connection = pymysql.connect(host='localhost',
+                             user='dht22',
+                             password='password',
+                             db='dht22',
+                             charset='utf8mb4',
+                             cursorclass=pymysql.cursors.DictCursor)
+            # Print latest record
+            with connection.cursor() as cursor:
+                sql = "SELECT * FROM `history` ORDER BY `id` DESC"
+                cursor.execute(sql)
+                result = cursor.fetchall()
+                print(result[0])
+
+            # Direct get value from sensor
+            # temperature_c = dhtDevice.temperature
+            # humidity = dhtDevice.humidity
+
         except RuntimeError as error:
+            # Error handle for Database fetch error
+            result = [{"dirtHum": -999, "hum": -999,"temp": -999,}]
+
             print(error.args[0]) # A full buffer was not returned. Try again.
             temperature_c = -999
             humidity = -999
 
-        return render_template('index.html', temp = temperature_c, humidity = humidity)
+        finally:
+            connection.close()
 
-    elif request.method == 'POST':
-        return render_template('index.html', post=False)
+        return render_template('index.html', result=result)
